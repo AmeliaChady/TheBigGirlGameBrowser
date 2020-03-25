6f42f9eeff2718a5a0bc68372fd0cffa6e4159ea
Rmelikyan <rmelikyan@ithaca.edu>
2020-03-25 18:43:38 -0400

    tests added

--- src/test/java/FakerTest.java
+++ src/test/java/FakerTest.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class FakerTest {
     @Test
     public void fakeTest(){
         assertEquals(4, Faker.fake(2));
+        assertEquals(6, Faker.fake(3));
+        assertFalse(4 == Faker.fake(7));
     }
-
 }
