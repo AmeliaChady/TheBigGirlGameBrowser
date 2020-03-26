90c759de764ebf87f373cea303b2ba89b1082a83
sradms0 <sradms0@openmailbox.org>
2020-03-25 21:59:25 -0400

    pass GameList constructor test

--- src/main/java/GameList.java
+++ src/main/java/GameList.java
@@ -6,7 +6,11 @@ public class GameList {
      * @param name - name of the game list
      * @throws IllegalArgumentException if name is invalid
      */
-    public GameList(String name) {}
+    public GameList(String name) throws IllegalArgumentException {
+        if (name.length() == 0)
+            throw new IllegalArgumentException("Please provide a name for this game list");
+        this.name = name;
+    }
 
     // ------GETTERS------
     public String getName() { return name; }
