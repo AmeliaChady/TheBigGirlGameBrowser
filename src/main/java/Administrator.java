0826e068535fb8b27d8a66d799ed2d3b2e237b61
sradms0 <sradms0@openmailbox.org>
2020-03-27 19:06:42 -0400

    add Administrator constructor test

---
+++ src/main/java/Administrator.java
@@ -0,0 +1,12 @@
+public class Administrator {
+    private String username;
+
+    /**
+     * Default constructor
+     * @param username - a username to identify the administrator
+     */
+    public Administrator(String username) throws IllegalArgumentException {}
+
+    // ------GETTERS------
+    public String getUsername() { return ""; }
+}
