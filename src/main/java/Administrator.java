1c12c91d24f3fa87c516da6257e86b5587c5b2cd
sradms0 <sradms0@openmailbox.org>
2020-03-27 19:10:01 -0400

    pass Administrator constructor test

--- src/main/java/Administrator.java
+++ src/main/java/Administrator.java
@@ -4,9 +4,14 @@ public class Administrator {
     /**
      * Default constructor
      * @param username - a username to identify the administrator
+     * @throws IllegalArgumentException if username is invalid
      */
-    public Administrator(String username) throws IllegalArgumentException {}
+    public Administrator(String username) throws IllegalArgumentException {
+        if (username.length() == 0)
+            throw new IllegalArgumentException("Please provide a name for this game list");
+        this.username = username;
+    }
 
     // ------GETTERS------
-    public String getUsername() { return ""; }
+    public String getUsername() { return username; }
 }
