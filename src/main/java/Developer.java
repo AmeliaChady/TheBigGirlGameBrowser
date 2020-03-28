f751a69468395782f2aaa42853fa03f2381e33de
Rmelikyan <rmelikyan@ithaca.edu>
2020-03-28 19:11:46 -0400

    Developer is restructured for database

--- src/main/java/Developer.java
+++ src/main/java/Developer.java
@@ -1,16 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Developer {
 
-    private int id;
     private String name;
+    private String gameListTitle;
     private GameList gameList;
+
 
     public Developer(String nameIn){
         name = nameIn;
-        gameList = new GameList(nameIn+"'s Games");
+        gameListTitle = nameIn+"'s Games";
+        gameList = new GameList(gameListTitle);
+    }
+
+    public Developer(String name, String gameListTitle, GameList gameList){
+        this.name = name;
+        this.gameListTitle = gameListTitle;
+        this.gameList = gameList;
     }
 
     public String getName(){
         return name;
+    }
+
+    public String getGameListTitle(){
+        return gameListTitle;
     }
 
     public GameList getGameList(){
--- src/test/java/DeveloperTests.java
+++ src/test/java/DeveloperTests.java
@@ -7,14 +7,17 @@ public class DeveloperTests {
     public void developerConstructorTest(){
         Developer d1 = new Developer("Bob (the cat)");
         assertNotNull(d1.getName());
-        assertNotNull(d1.getGameList());
-        assertEquals(0, d1.getGameList().getGameCount());
+        assertNotNull(d1.getGameListTitle());
+        assertEquals("Bob (the cat)'s Games", d1.getGameListTitle());
         assertEquals("Bob (the cat)", d1.getName());
+        assertEquals(0, d1.getGameList().getGameCount());
+
 
         Developer d2 = new Developer("Bob (my dad)");
         assertNotNull(d2.getName());
-        assertNotNull(d2.getGameList());
-        assertEquals(0, d2.getGameList().getGameCount());
+        assertNotNull(d2.getGameListTitle());
+        assertEquals("Bob (my dad)'s Games", d2.getGameListTitle());
         assertEquals("Bob (my dad)", d2.getName());
+        assertEquals(0, d2.getGameList().getGameCount());
     }
 }
