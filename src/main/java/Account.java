ed653c58b5377e9fefaf56dc38f9d364a4fdc03a
kerrybuckman <kbuckman@ithaca.edu>
2020-04-10 09:36:46 -0400

    account constructor written and isEmailValid added

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -4,8 +4,18 @@ public class Account {
     private String email;
     private String password;
 
-    public Account(String usernameIn, String emailIn, String passwordIn){
+    public static final String prefix_invalid_endings = "-._";
+    public static final String invalid_overall = "!#$%^&*()+=,<>/?;:'\"[{]}\\|";
 
+    public Account(String usernameIn, String emailIn, String passwordIn){
+        if (isEmailValid(emailIn)){
+            username = usernameIn;
+            email = emailIn;
+            password = passwordIn;
+        }
+        else{
+            throw new IllegalArgumentException(emailIn + " is not a valid email address");
+        }
     }
 
     //getters and setters
@@ -35,6 +45,42 @@ public class Account {
     //end getters and setters
 
     public static boolean isEmailValid(String email){
-        return false;
+        if(email.split("@").length != 2){
+            return false;
+        }
+
+        for(char i : invalid_overall.toCharArray()){
+            if(email.contains(i+"")){
+                return false;
+            }
+        }
+
+        String[] a = email.split("@");
+        String prefix = a[0];
+        String suffix = a[1];
+
+        /// Prefix Tests
+        if(prefix.length()==0){
+            return false;
+        }
+
+        for(char i : prefix_invalid_endings.toCharArray()){
+            if(prefix.endsWith(i+"")){
+                return false;
+            }
+        }
+
+        /// Suffix Tests
+        String[] suffixsplit = suffix.split("\\.");
+
+        if(suffixsplit.length < 2){
+            return false;
+        }
+
+        if(suffixsplit[suffixsplit.length - 1].length() < 2) {
+            return false;
+        }
+
+        return true;
     }
 }
