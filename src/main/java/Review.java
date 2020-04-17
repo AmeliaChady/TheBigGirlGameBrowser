98d30420f8ed4e74413a6482cbd5c74a495a4f34
kerrybuckman <kbuckman@ithaca.edu>
2020-04-17 12:33:33 -0400

    constructor test

---
+++ src/main/java/Review.java
@@ -0,0 +1,24 @@
+public class Review {
+    private int rating;
+    private String comment;
+
+    public Review(int rateIn, String commentIn){
+
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
