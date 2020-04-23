3a50790c7b72329000823f5bd418ce9e0998abf2
AmeliaChady <6AnGp9C*mFdj>
2020-04-23 15:48:32 -0400

    demo stuff

--- src/main/java/UISprint2.java
+++ src/main/java/UISprint2.java
@@ -495,7 +495,7 @@ public class UISprint2 {
 
 
     public static void main(String[] args) throws IOException, ParseException, DataSourceException {
-        UISprint2 myBGGLTest = new UISprint2("src/databases/Test_SQLiteSource/Login.db");
+        UISprint2 myBGGLTest = new UISprint2("src/databases/DemoDaySprint2.db");
         myBGGLTest.login();
 
     }
